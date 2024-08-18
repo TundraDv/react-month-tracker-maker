d9b6ef70778d012082f965bb0655552844ada64e
tundraElie <tundra.elie@gmail.com>
2024-08-17 21:15:38 -0400

    margins

--- react-month-tracker-maker/src/Components/Goals.jsx
+++ react-month-tracker-maker/src/Components/Goals.jsx
@@ -17,7 +17,7 @@ function Goals( {goalsSettings} ) {
     <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', marginY: 1 }}>
       <Box
       sx={{
-        marginX: 3,
+        marginX: (columns < 4) ? 4 : 3,
         width: "100%",
         // borderRadius: 4,
         // border: '10px groove  rgba(0, 0, 0, 0.5);',
@@ -25,13 +25,13 @@ function Goals( {goalsSettings} ) {
         // backgroundColor: "#fff"
       }}
     >
-      <Grid container spacing={0.1} justifyContent="center" alignItems="center">
+      <Grid container spacing={0.7} justifyContent="center" alignItems="center">
         {Array.from({ length: totalItems }).map((_, index) => (
           <Grid item xs={12 / columns} key={index}>
             {/[^\s]/.test(textfields[index]) ? (
               <Stack
                 direction="row"
-                spacing={0.1}
+                spacing={0.2}
                 alignItems="center"
                 justifyContent="flex-start"
                 sx={{ width: '100%' }}
